2c4c7338974a8996c1a468bb2274ffcf92f495d9
unknown <skandalers@yahoo.com.mx>
2020-05-03 03:07:34 -0500

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import {EmptyState, Layout, Page } from '@shopify/polaris';
+
+
+function Index(){
+    return(
+        <div>
+            <Page>
+                <Layout>
+                    <EmptyState
+                     heading="Manage your inventory transfers"
+                     action={{content: 'Select Products'}}
+                     secondaryAction={{content: 'Learn more', url: 'https://help.shopify.com'}}
+                     image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+                     >
+                        <p>Select Products</p>
+                    </EmptyState>
+                </Layout>
+            </Page>
+        </div>
+    )
+}
+
+export default Index;
